5c91f95c5e3a9e50e776df031d8c3966b871c8da
andev <andh1006@naver.com>
2025-11-04 15:29:05 +0900

    Initial Commit

---
+++ FlatBread/FlatBreadApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlatBreadApp.swift
+//  FlatBread
+//
+//  Created by andev on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct FlatBreadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
