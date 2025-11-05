4e09eb7b7c42c11267c31ced7fbd85e96fd0e53f
pHH <gudghks56@gmail.com>
2025-11-05 23:10:44 +0900

    feat: API 기본 key값 namespace 생성

---
+++ FlatBread/Core/Network/Helper/APIConfig.swift
@@ -0,0 +1,14 @@
+//
+//  APIConfig.swift
+//  FlatBread
+//
+//  Created by hwan on 11/5/25.
+//
+
+import Foundation
+
+enum APIConfig {
+    static let apikey: String = ""
+    static let productID: String = ""
+    static let baseURL: String = "v1"
+}
