b6d0cb8ca3a86168ab6c57ef791b33e5c0aae6f2
pHH <gudghks56@gmail.com>
2025-11-07 11:53:44 +0900

    refactor: APIConfig - info.plist key 값 추가, transport setting

--- FlatBread/Core/Network/Helper/APIConfig.swift
+++ FlatBread/Core/Network/Helper/APIConfig.swift
@@ -8,7 +8,21 @@
 import Foundation
 
 enum APIConfig {
-    static let apikey: String = ""
-    static let productID: String = ""
-    static let baseURL: String = "v1"
+    private static let info = Bundle.main.infoDictionary
+    private static let domain: String = if let domain = info?["domain"] as? String {
+        domain
+    } else {
+        fatalError("required base_url")
+    }
+    static let apikey: String = if let api_key = info?["api_key"] as? String {
+        api_key
+    } else {
+        fatalError("required api_key")
+    }
+    static let productID: String = if let id = info?["product_id"] as? String {
+        id
+    } else {
+        fatalError("required product_id")
+    }
+    static let baseURL: String = "http://" + domain + "/v1"
 }
