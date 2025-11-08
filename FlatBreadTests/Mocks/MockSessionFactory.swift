b7656a5a73e142534050f9f6b87188147ad13574
pHH <gudghks56@gmail.com>
2025-11-08 17:25:36 +0900

    test: MockSession Factory 추가

---
+++ FlatBreadTests/Mocks/MockSessionFactory.swift
@@ -0,0 +1,41 @@
+//
+//  MockSession.swift
+//  FlatBreadTests
+//
+//  Created by hwan on 11/7/25.
+//
+
+import Foundation
+import Alamofire
+@testable import FlatBread
+
+final class MockSessionFactory {
+    
+    var tokenCoordinator: TokenRefreshCoordinator
+    var tokenStorage: MockTokenStorage
+    var session: Session
+    
+    init(accessToken: String, refreshToken: String) {
+        let mainSession = Self.createMockSession()
+        self.tokenStorage = MockTokenStorage(accessToken: accessToken, refreshToken: refreshToken)
+        self.tokenCoordinator = TokenRefreshCoordinator(
+            tokenStorage: tokenStorage,
+            session: mainSession
+        )
+        self.session = mainSession
+    }
+    
+    static func createMockSession() -> Session {
+        let configuration = URLSessionConfiguration.af.ephemeral
+        configuration.protocolClasses = [MockURLProtocol.self]
+        return Session(configuration: configuration)
+    }
+    
+    func makeNetworkService() -> AsyncNetworkService {
+        DefaultNetworkService(
+            session: session,
+            tokenInterceptor: TokenInterceptor(coordinator: tokenCoordinator),
+            networkRetrier: NetworkRetryRetrier()
+        )
+    }
+}
