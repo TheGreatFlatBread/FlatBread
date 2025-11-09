961877571f24401e8c5373656cf42e1934a72d5c
Minsung Kim <mskim4048@naver.com>
2025-11-09 19:55:30 +0900

    chore: ContentView 코드 원상복구

--- FlatBread/App/ContentView.swift
+++ FlatBread/App/ContentView.swift
@@ -6,37 +6,16 @@
 //
 
 import SwiftUI
-import UIKit
 
 struct ContentView: View {
-    
-    init() {
-        let defaultAppearance = UITabBarAppearance()
-        defaultAppearance.configureWithDefaultBackground()
-        UITabBar.appearance().standardAppearance = defaultAppearance
-        UITabBar.appearance().scrollEdgeAppearance = defaultAppearance
-    }
-    
     var body: some View {
-        TabView {
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
-            }
-            .padding()
-            .tabItem {
-                Image(systemName: "house")
-                Text("홈")
-            }
-            
-            MainMapView()
-                .tabItem {
-                    Image(systemName: "map")
-                    Text("지도")
-                }
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
+        .padding()
     }
 }
 
