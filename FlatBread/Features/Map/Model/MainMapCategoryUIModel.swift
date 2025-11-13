0474380cc5e8a00d648a80ac5ca03ea45a81f263
Minsung Kim <mskim4048@naver.com>
2025-11-13 18:15:02 +0900

    feat: 지도 뷰 카테고리 버튼 컴포넌트 구현

---
+++ FlatBread/Features/Map/Model/MainMapCategoryUIModel.swift
@@ -0,0 +1,13 @@
+//
+//  MainMapCategoryUIModel.swift
+//  FlatBread
+//
+//  Created by 김민성 on 11/11/25.
+//
+
+struct MainMapCategoryUIModel: Hashable {
+    let name: String
+    /// system image name
+    let image: String
+    var isSelected: Bool = true
+}
