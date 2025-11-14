2a0b71abbe1c424e564f9044b079f0946c43771f
Minsung Kim <mskim4048@naver.com>
2025-11-14 22:42:04 +0900

    feat: 탭바 기본 틀 구현

--- FlatBread/App/ContentView.swift
+++ FlatBread/App/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        // tabItem modifier는 deprecated되었으나,
+        // 그 대체제인 Tab은 iOS 18.0 이상부터 사용 가능하므로
+        // tabItem modifier를 사용하였음.
+        TabView {
+            Color(.green)
+                .ignoresSafeArea()
+                .tabItem {
+                    Label("홈", systemImage: "house")
+                }
+            
+            Color(.blue)
+                .ignoresSafeArea()
+                .tabItem {
+                    Label("숏폼", systemImage: "play.rectangle")
+                }
+            
+            MainMapView()
+                .tabItem {
+                    Label("지도", systemImage: "map")
+                }
+            
+            Color(.yellow)
+                .ignoresSafeArea()
+                .tabItem {
+                    Label("지도", systemImage: "checkmark.rectangle.stack")
+                }
+            
+            Color(.orange)
+                .ignoresSafeArea()
+                .tabItem {
+                    Label("지도", systemImage: "person")
+                }
         }
-        .padding()
+        .tint(.black)
     }
 }
 
