f21f776734a3a10d397c994145ad93ad2957eea3
pHH <gudghks56@gmail.com>
2025-11-15 21:34:53 +0900

    feat: CameraImage Picker View - Representable 로 구현

---
+++ FlatBread/Features/Chat/Presentation/Components/CameraImagePicker.swift
@@ -0,0 +1,49 @@
+//
+//  CameraImagePicker.swift
+//  FlatBread
+//
+//  Created by hwan on 11/14/25.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct CameraImagePicker: UIViewControllerRepresentable {
+    @Environment(\.dismiss) private var dismiss
+    var action: ((UIImage) -> Void)?
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: CameraImagePicker
+
+        init(_ parent: CameraImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(
+            _ picker: UIImagePickerController,
+            didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+        ) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.action?(image)
+            }
+            parent.dismiss()
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.dismiss()
+        }
+    }
+}
