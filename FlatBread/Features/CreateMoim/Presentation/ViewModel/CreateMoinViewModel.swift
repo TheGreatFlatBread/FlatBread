012cf6b15e5e2d2c2946b4ea8a28977a7456fabc
Minsung Kim <mskim4048@naver.com>
2025-11-18 18:25:43 +0900

    chore: 기존 MoimCategory 이름을 MoimCategory2로 변경

--- FlatBread/Features/CreateMoim/Presentation/ViewModel/CreateMoinViewModel.swift
+++ FlatBread/Features/CreateMoim/Presentation/ViewModel/CreateMoinViewModel.swift
@@ -29,8 +29,8 @@ final class CreateMoimViewModel: ObservableObject {
     )
 
     // UI 상태
-    @Published var categories: [MoimCategory] = .dummy10
-    @Published var selectedCategory: MoimCategory? = nil
+    @Published var categories: [MoimCategory2] = .dummy10
+    @Published var selectedCategory: MoimCategory2? = nil
     @Published var isFree: Bool = true
     @Published var priceText: String = ""      // 유료일 때만 사용(숫자 문자열)
     @Published var selectedImageData: Data? = nil
@@ -70,7 +70,7 @@ final class CreateMoimViewModel: ObservableObject {
         return !t.isEmpty && !c.isEmpty && !(dto.category ?? "").isEmpty && (dto.price ?? 0) >= 0
     }
 
-    func selectCategory(_ cat: MoimCategory) {
+    func selectCategory(_ cat: MoimCategory2) {
         selectedCategory = cat
         dto.category = cat.name
     }
