fc578a9622fe8f59f33152defb8e7e583dc0d207
Minsung Kim <mskim4048@naver.com>
2025-11-18 10:29:32 +0900

    feat: 검색창 focusing 기능 구현

--- FlatBread/Features/Map/Presentation/MainMapView.swift
+++ FlatBread/Features/Map/Presentation/MainMapView.swift
@@ -12,6 +12,9 @@ import NMapsGeometry
 struct MainMapView: View {
     
     @StateObject private var viewModel: MainMapViewModel
+    @FocusState private var searchBarFocused: Bool
+    @State private var isSearchActive: Bool = false
+//    @Namespace private var animation
     
     init(initialPosition: NMGLatLng = .init(lat: 37.517677, lng: 126.886442)) {
         let viewModel = MainMapViewModel(coordinate: initialPosition)
@@ -27,33 +30,6 @@ struct MainMapView: View {
                     focusingPlaceID: $viewModel.focusingPlaceID
                 )
                 .ignoresSafeArea(.all)
-                
-                VStack {
-                    MainMapSearchBar(searchText: $viewModel.searchText)
-                    ScrollView(.horizontal) {
-                        HStack {
-                            ForEach($viewModel.categories, id: \.name) { category in
-                                MainMapCategoryButton(category: category)
-                            }
-                        }
-                        .padding(.vertical, 1.5)
-                        .padding(.horizontal)
-                    }
-                    .scrollIndicators(.hidden)
-                    
-                    Spacer()
-                }
-                .background(
-                    LinearGradient(
-                        colors: [.white.opacity(0.95), .white.opacity(0)],
-                        startPoint: .top,
-                        endPoint: .bottom
-                    )
-                    .frame(height: 150)
-                    .edgesIgnoringSafeArea(.top)
-                    .allowsHitTesting(false),
-                    alignment: .top
-                )
                 
                 VStack(spacing: 0) {
                     Spacer()
@@ -83,6 +59,67 @@ struct MainMapView: View {
                         }
                     }
                 }
+                .ignoresSafeArea(.keyboard, edges: .bottom)
+                
+                ZStack {
+                    if isSearchActive {
+                        Color(UIColor.systemGray6)
+                            .ignoresSafeArea()
+                    }
+                    
+                    VStack {
+                        HStack(spacing: 12) {
+                            MainMapSearchBar(searchText: $viewModel.searchText)
+                                .focused($searchBarFocused)
+                            
+                            if isSearchActive {
+                                Button {
+                                    withAnimation {
+                                        searchBarFocused = false
+                                    }
+                                } label: {
+                                    Image(systemName: "xmark")
+                                        .font(.system(size: 17))
+                                        .padding(12)
+                                        .background(Color.white)
+                                        .foregroundStyle(.gray)
+                                        .cornerRadius(.infinity)
+                                        .shadow(radius: 8)
+                                }
+                            }
+                        }
+                        .padding(.horizontal, 12)
+                        
+                        ScrollView(.horizontal) {
+                            HStack {
+                                ForEach($viewModel.categories, id: \.name) { category in
+                                    MainMapCategoryButton(category: category)
+                                }
+                            }
+                            .padding(.vertical, 1.5)
+                            .padding(.horizontal)
+                        }
+                        .scrollIndicators(.hidden)
+                        
+                        Spacer()
+                    }
+                    .background(
+                        LinearGradient(
+                            colors: [.white.opacity(0.95), .white.opacity(0)],
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
+                        .frame(height: 150)
+                        .edgesIgnoringSafeArea(.top)
+                        .allowsHitTesting(false),
+                        alignment: .top
+                    )
+                }
+            }
+        }
+        .onChange(of: searchBarFocused) { oldValue, newValue in
+            withAnimation {
+                isSearchActive = newValue
             }
         }
         .task {
