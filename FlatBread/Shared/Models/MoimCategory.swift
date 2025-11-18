09b2552fb75dd29e9f603b29d6704ca64e440fdb
Minsung Kim <mskim4048@naver.com>
2025-11-18 18:27:03 +0900

    feat: MoimCategory 정의
    앱 전역에서 사용될 모입의 Category 타입

---
+++ FlatBread/Shared/Models/MoimCategory.swift
@@ -0,0 +1,27 @@
+//
+//  MoimCategory.swift
+//  FlatBread
+//
+//  Created by 김민성 on 11/18/25.
+//
+
+enum MoimCategory: String, Hashable, CaseIterable {
+    case exerciseSports = "운동/스포츠"
+    case selfImprovement = "자기계발"
+    case humanitiesBooksWriting = "인문학/책/글"
+    case culturePerformancesFestivals = "문화/공연/축제"
+    case craftsMaking = "공예/만들기"
+    case volunteerWork = "봉사활동"
+    case carsBikes = "차/바이크"
+    case watchingSports = "스포츠관람"
+    case cookingManufacturing = "요리/제조"
+    case foreignLanguage = "외국/언어"
+    case outdoorTravel = "아웃도어/여행"
+    case industryJobRole = "업종/직무"
+    case musicInstruments = "음악/악기"
+    case danceBallet = "댄스/무용"
+    case socialNetworking = "사교/인맥"
+    case photographyVideo = "사진/영상"
+    case gamesEntertainment = "게임/오락"
+    case pets = "반려동물"
+}
