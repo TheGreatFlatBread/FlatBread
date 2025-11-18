1e196e289011f0aa59326d02c89f1dd85361f63b
andev <andh1006@naver.com>
2025-11-18 18:56:44 +0900

    fix: LoginViewModel isLoginSucceed 위치 변경

--- FlatBread/Features/Auth/LoginViewModel.swift
+++ FlatBread/Features/Auth/LoginViewModel.swift
@@ -54,7 +54,6 @@ final class LoginViewModel: NSObject, ObservableObject {
             
             Task {
                 await appleLogin(idToken: idToken)
-                isLoginSucceed = true
             }
             
             
@@ -90,7 +89,10 @@ final class LoginViewModel: NSObject, ObservableObject {
             #if DEBUG
             print("accessToken: \(signInInfo.accessToken!)")
             #endif
+            
+            isLoginSucceed = true
         } catch {
+            print("\(error)")
             alertMessage = error.localizedDescription
             showingAlert = true
         }
