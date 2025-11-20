4f1b8c0e9548f1d9c16b210e6ee663554401c044
andev <andh1006@naver.com>
2025-11-20 20:27:16 +0900

    chore: HomeCategoryDetailView 임시 추가

---
+++ FlatBread/Features/HomeCategoryDetail/Presentation/View/HomeCategoryDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeCategoryDetailView.swift
+//  FlatBread
+//
+//  Created by andev on 11/20/25.
+//
+
+import SwiftUI
+
+struct HomeCategoryDetailView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    HomeCategoryDetailView()
+}
